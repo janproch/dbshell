00b4dd5ff2d95eda675cb88f608e1152e73a0330
Jan Prochazka <jan.prochazka@gmail.com>
2013-04-20 13:15:36 +0200

    subquery expression

--- DbShell.Driver.Common/DmlFramework/DmlfExpression.cs
+++ DbShell.Driver.Common/DmlFramework/DmlfExpression.cs
@@ -360,4 +360,21 @@ namespace DbShell.Driver.Common.DmlFramework
             return "lit";
         }
     }
+
+    public class DmlfSubSelectExpression : DmlfExpression
+    {
+        public DmlfSelect Select { get; set; }
+
+        protected override string GetTypeName()
+        {
+            return "subselect";
+        }
+
+        public override void GenSql(ISqlDumper dmp, IDmlfHandler handler)
+        {
+            dmp.Put("(");
+            Select.GenSql(dmp, handler);
+            dmp.Put(")");
+        }
+    }
 }
