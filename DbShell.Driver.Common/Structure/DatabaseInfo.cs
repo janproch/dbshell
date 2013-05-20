b521b8e60f675864e8ade4561ef643f6fe3922b8
Jan Prochazka <jan.prochazka@gmail.com>
2013-05-20 23:28:26 +0200

    findprogrammable method

--- DbShell.Driver.Common/Structure/DatabaseInfo.cs
+++ DbShell.Driver.Common/Structure/DatabaseInfo.cs
@@ -26,38 +26,73 @@ namespace DbShell.Driver.Common.Structure
         public List<FunctionInfo> Functions { get { return _functions; } }
 
 
+        private T FindObject<T>(IEnumerable<T> objs, string name )
+            where T : NamedObjectInfo
+        {
+            return objs.FirstOrDefault(t => String.Compare(t.Name, name, true) == 0);
+        }
+
+        private T FindObject<T>(IEnumerable<T> objs, string schema, string name)
+            where T : NamedObjectInfo
+        {
+            if (schema == null)
+            {
+                return objs.FirstOrDefault(t => String.Compare(t.Name, name, true) == 0);
+            }
+            else
+            {
+                return objs.FirstOrDefault(t => String.Compare(t.Name, name, true) == 0 && String.Compare(t.Schema, schema, true) == 0);
+            }
+        }
+
         public TableInfo FindTable(string table)
         {
-            return _tables.FirstOrDefault(t => String.Compare(t.Name, table, true) == 0);
+            return FindObject(_tables, table);
         }
 
         public TableInfo FindTable(string schema, string table)
         {
-            if (schema == null)
-            {
-                return _tables.FirstOrDefault(t => String.Compare(t.Name, table, true) == 0);
-            }
-            else
-            {
-                return _tables.FirstOrDefault(t => String.Compare(t.Name, table, true) == 0 && String.Compare(t.Schema, schema, true) == 0);
-            }
+            return FindObject(_tables, schema, table);
         }
 
         public ViewInfo FindView(string view)
         {
-            return _views.FirstOrDefault(t => String.Compare(t.Name, view, true) == 0);
+            return FindObject(_views, view);
         }
 
         public ViewInfo FindView(string schema, string view)
         {
-            if (schema == null)
+            return FindObject(_views, schema, view);
+        }
+
+        public ProgrammableInfo FindProgrammable(string name, bool procedure = true, bool function = true)
+        {
+            if (procedure)
             {
-                return _views.FirstOrDefault(t => String.Compare(t.Name, view, true) == 0);
+                var res = FindObject(_storedProcedures, name);
+                if (res != null) return res;
             }
-            else
+            if (function)
             {
-                return _views.FirstOrDefault(t => String.Compare(t.Name, view, true) == 0 && String.Compare(t.Schema, schema, true) == 0);
+                var res = FindObject(_functions, name);
+                if (res != null) return res;
             }
+            return null;
+        }
+
+        public ProgrammableInfo FindProgrammable(string schema, string name, bool procedure = true, bool function = true)
+        {
+            if (procedure)
+            {
+                var res = FindObject(_storedProcedures, schema, name);
+                if (res != null) return res;
+            }
+            if (function)
+            {
+                var res = FindObject(_functions, schema, name);
+                if (res != null) return res;
+            }
+            return null;
         }
 
         //public ColumnInfo FindColumn(string table, string column)
