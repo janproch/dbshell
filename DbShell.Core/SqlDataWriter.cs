5f301f0926ae3e82298816fea757b8f958bbf60a
Jan Prochazka <jan.prochazka@gmail.com>
2014-01-28 16:45:55 +0100

    SQL data writer

---
+++ DbShell.Core/SqlDataWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DbShell.Common;
+using DbShell.Core.Utility;
+using DbShell.Driver.Common.AbstractDb;
+using DbShell.Driver.Common.CommonDataLayer;
+using DbShell.Driver.Common.Structure;
+
+namespace DbShell.Core
+{
+    public class SqlDataWriter : ElementBase, ITabularDataTarget
+    {
+        /// <summary>
+        ///  name of input file
+        /// </summary>
+        public string File { get; set; }
+
+        public bool AvailableRowFormat
+        {
+            get { return false; }
+        }
+
+        public ICdlWriter CreateWriter(TableInfo rowFormat, CopyTableTargetOptions options)
+        {
+            string file = Context.ResolveFile(Replace(File), ResolveFileMode.Output);
+            var fw = new StreamWriter(file);
+            return new SqlFileWriter(fw, Context.DefaultConnection.Factory);
+        }
+
+        public TableInfo GetRowFormat()
+        {
+            return null;
+        }
+    }
+}
---
+++ DbShell.Core/Utility/SqlFileWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DbShell.Driver.Common.AbstractDb;
+using DbShell.Driver.Common.CommonDataLayer;
+using DbShell.Driver.Common.Utility;
+
+namespace DbShell.Core.Utility
+{
+    public class SqlFileWriter : ICdlWriter
+    {
+        private TextWriter _stream;
+        private IDatabaseFactory _factory;
+
+        public SqlFileWriter(TextWriter stream, IDatabaseFactory factory)
+        {
+            _stream = stream;
+            _factory = factory;
+        }
+
+        public void Dispose()
+        {
+            if (Disposing != null)
+            {
+                Disposing();
+                Disposing = null;
+            }
+            _stream.Dispose();
+        }
+
+        public event Action Disposing;
+
+        public void Write(ICdlRecord row)
+        {
+            var fmt = _factory.CreateLiteralFormatter();
+            _stream.Write("INSERT INTO [TABLE_NAME] (");
+            _stream.Write(row.GetFieldNames().Select(x => "[" + x + "]").CreateDelimitedText(", "));
+            _stream.Write(") VALUES (");
+            for (int i = 0; i < row.FieldCount; i++)
+            {
+                if (i > 0) _stream.Write(", ");
+                row.ReadValue(i);
+                fmt.ReadFrom(row);
+                _stream.Write(fmt.GetText());
+            }
+            _stream.Write(");\n");
+        }
+    }
+}
