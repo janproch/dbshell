198a8989e210d2b38380df8b87f9b8adefa98a2f
Jan Prochazka <jan.prochazka@gmail.com>
2015-06-29 18:47:38 +0200

    bugfixes

--- DbShell.Driver.Common/CommonDataLayer/CdlDataColumnInfo.cs
+++ DbShell.Driver.Common/CommonDataLayer/CdlDataColumnInfo.cs
@@ -10,5 +10,14 @@ namespace DbShell.Driver.Common.CommonDataLayer
     {
         public NameWithSchema TableName;
         public string TableColumnName;
+
+        public bool MatchTableName(NameWithSchema tableName)
+        {
+            if (tableName == null) return false;
+            if (TableName == null) return false;
+            if (TableName.Schema == null) return System.String.Compare(TableName.Name ?? "", tableName.Name ?? "", System.StringComparison.OrdinalIgnoreCase) == 0;
+            return String.Compare(TableName.Schema ?? "", tableName.Schema ?? "", System.StringComparison.OrdinalIgnoreCase) == 0
+                   && System.String.Compare(TableName.Name ?? "", tableName.Name ?? "", System.StringComparison.OrdinalIgnoreCase) == 0;
+        }
     }
 }
