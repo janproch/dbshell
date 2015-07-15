651bddd71dccbc6163b98615cff45d0727f8e844
Jan Prochazka <jan.prochazka@gmail.com>
2015-07-15 08:06:54 +0200

    download functionality (only basic skeleton)

---
+++ DbShell.Core/Download.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using DbShell.Common;
+using DbShell.Core.Utility;
+using DbShell.Driver.Common.Utility;
+
+namespace DbShell.Core
+{
+    /// <summary>
+    /// Writes string or binary data to file or to variable
+    /// </summary>
+    public class Download : RunnableBase
+    {
+        /// <summary>
+        /// Gets or sets the file name
+        /// </summary>
+        [XamlProperty]
+        public string File { get; set; }
+
+        /// <summary>
+        /// URL to be downloaded
+        /// </summary>
+        [XamlProperty]
+        public string Url { get; set; }
+
+        protected override void DoRun(IShellContext context)
+        {
+            string file = context.ResolveFile(context.Replace(File), ResolveFileMode.Output);
+            string url = context.Replace(Url);
+            context.OutputMessage("Downloading from URL " + url);
+            var client = new WebClient();
+            client.DownloadFile(url, file);
+        }
+    }
+}
