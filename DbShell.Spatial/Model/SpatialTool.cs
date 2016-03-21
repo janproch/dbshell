36abc07cadf7b448e7dc5e7506d08ef2fcd7669d
Jan Prochazka <jan.prochazka@gmail.com>
2016-03-21 23:46:49 +0100

    spatial tools, shapefile reader

---
+++ DbShell.Spatial/Model/SpatialTool.cs
@@ -0,0 +1,20 @@
+﻿using DbShell.Common;
+using DotSpatial.Projections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DbShell.Spatial.Model
+{
+    public static class SpatialTool
+    {
+        public static ProjectionInfo GetProjection(string name, IShellContext context)
+        {
+            if (name == null) return null;
+            name = context.Replace(name);
+            if (name == null) return null;
+            return KnownCoordinateSystems.Geographic.World.GetProjection(name);
+        }
+    }
+}
