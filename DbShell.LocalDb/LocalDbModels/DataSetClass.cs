9a5b1ebeac477aa9e296f80d23176919c464f68c
Jan Prochazka <jan.prochazka@gmail.com>
2017-03-15 18:08:12 +0100

    local data set

---
+++ DbShell.LocalDb/LocalDbModels/DataSetClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DbShell.LocalDb.LocalDbModels
+{
+    public class DataSetClass
+    {
+        public string Name;
+        public int RowCount;
+    }
+}
