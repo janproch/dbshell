bdfae70af3a7d0f0dacb497ceaf4c4f0f5c033fc
Jan Prochazka <jan.prochazka@gmail.com>
2018-04-22 19:54:36 +0200

    missing files

---
+++ DbShell.Driver.Common/Utility/ServiceProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbShell.Driver.Common.Utility
+{
+    public static class ServiceProviderExtensions
+    {
+        public static T GetCachedService<T>(this IServiceProvider serviceProvider, ref T cachedValue)
+        {
+            if (cachedValue != null)
+                return cachedValue;
+
+            cachedValue = serviceProvider.GetRequiredService<T>();
+            return cachedValue;
+        }
+    }
+}
---
+++ DbShell.Driver.Sqlite/SQliteStorageSubset.cs
@@ -0,0 +1,48 @@
+﻿using DbShell.Driver.Common.CommonDataLayer;
+using DbShell.Driver.Common.Interfaces;
+using DbShell.Driver.Common.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbShell.Driver.Sqlite
+{
+    public class SqliteStorageSubset : ITabularDataSource, IModelProvider
+    {
+        public string Identifier { get; set; }
+
+        public string Query { get; set; }
+
+        public int[] ColumnSubset { get; set; }
+
+        private Driver.Sqlite.SqliteStorage Storage
+        {
+            get { return Driver.Sqlite.SqliteStorage.GetFromDirectory(Identifier); }
+        }
+
+        DataFormatSettings ITabularDataSource.GetSourceFormat(IShellContext context)
+        {
+            return null;
+        }
+
+        TableInfo ITabularDataSource.GetRowFormat(IShellContext context)
+        {
+            return Storage.Structure.CreateColumnSubset(ColumnSubset);
+        }
+
+        ICdlReader ITabularDataSource.CreateReader(IShellContext context)
+        {
+            return Storage.CreateReader(Query, ColumnSubset);
+        }
+
+        object IModelProvider.GetModel(IShellContext context)
+        {
+            return this;
+        }
+
+        void IModelProvider.InitializeTemplate(IRazorTemplate template, IShellContext context)
+        {
+            template.TabularData = this;
+        }
+    }
+}
